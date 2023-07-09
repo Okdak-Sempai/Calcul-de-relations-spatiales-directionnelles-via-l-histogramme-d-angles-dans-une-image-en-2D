c0654068487647b62f16dd621f60a58cdeb91155
Okdak-Sempai <59941239+Okdak-Sempai@users.noreply.github.com>
2023-07-09 19:22:17 +0200

    Add files via upload

---
+++ src/projet/java/Couleurs.java
@@ -0,0 +1,16 @@
+package projet.java;
+
+/**
+ * Classe de constantes de couleurs.
+ *
+ * @see Objet#sauvegarde(String, int)
+ *
+ * @version 1.0.1
+ */
+public class Couleurs
+{
+    //Définition des couleurs par leurs valeurs entières (constante).
+    public  static final int VIOLET = -12320428;
+    public  static final int JAUNE = -137436;
+    public  static final int VERT = -14506108;
+}
